d8f2a6dcd70b7f5c67f450d7ae84956bd389737a
Anne Ruskamp <aneilsen@beehiveindustries.com>
2015-03-20 08:44:27 -0500

    initial set-up

---
+++ databasechecksdemo/DataContracts/DatabaseReleaseCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databasechecksdemo.DataContracts
+{
+    class DatabaseReleaseCheck
+    {
+        public long Id { get; set; }
+        public string SqlCheck { get; set; }
+        public string Expected { get; set; }
+        public string Operator { get; set; }
+        public string Severity { get; set; }
+        public string Name { get; set; }
+        public string Actual { get; set; }
+
+        public override string ToString()
+        {
+            var actual = (Actual == null) ? string.Empty : Actual;
+            var dbReleaseCheckStr = string.Format(@"{0}_{1} : {2} {3} {4}", Id, Name, Expected, Operator, actual);
+            return dbReleaseCheckStr;
+        }
+    }
+}
